7c0b9f75f2f86dcdfa1c12cdbf6df4ef10ce6a91
Brooke Bryan <brooke@bajb.net>
2019-08-28 19:32:04 +0100

    Generated

--- generate.sh
+++ generate.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 
-protoc -I proto \
+mkdir genproto
+cp proto/*.proto genproto/
+sed -i '' "s#github.com/chargehive/proto/proto/##g" genproto/*.proto
+
+protoc -I genproto \
 -I $GOPATH/src \
 --gogo_out=plugins=grpc,paths=source_relative:golang/chtype \
---proto_path=$GOPATH/src/github.com/chargehive/proto \
-proto/*.proto
+genproto/*.proto
 #--php_out=plugins=grpc,paths=source_relative:php \
+
+rm -Rf genproto
