e1d8e9d390ed00d904e396a723deee626f6ccf12
Tom Kay <oridan82@gmail.com>
2019-10-08 14:50:01 +0100

    add protoc check

--- generate.sh
+++ generate.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+set -e
+
+# dependency check
+PROTOC_PATH=$(command -v protoc 2>&1)
+if [[ "$PROTOC_PATH" == "" ]]; then
+  echo "'protoc' tool is required but missing." >&2
+  exit 1
+fi
+
 # init
 mkdir genproto
 cp proto/*.proto genproto/
@@ -8,7 +17,7 @@ cp proto/*.proto genproto/
 rm -rf golang
 mkdir -p golang/chtype
 sed -i '' "s#github.com/chargehive/proto/proto/##g" genproto/*.proto
-protoc -I genproto \
+$PROTOC_PATH -I genproto \
   -I $GOPATH/src \
   --gogo_out=plugins=grpc,paths=source_relative:golang/chtype \
   genproto/*.proto
@@ -18,7 +27,7 @@ rm -rf php
 mkdir -p php/src
 sed -i '' -E 's#^import "github.com/gogo/protobuf/gogoproto/gogo.proto";$##g' genproto/*.proto
 sed -i '' -E 's#^option \(gogoproto\..+##g' genproto/*.proto
-protoc -I genproto \
+$PROTOC_PATH -I genproto \
   -I $GOPATH/src \
   --php_out=php/src \
   genproto/*.proto
