3a79a6d71e926a54ec886fa826b96d1bf5570610
Tom Kay <oridan82@gmail.com>
2021-08-05 16:18:54 +0100

    regenerate

--- generate.sh
+++ generate.sh
@@ -10,26 +10,24 @@ if [[ "$PROTOC_PATH" == "" ]]; then
 fi
 
 # go
-rm -rf golang
-mkdir -p golang
+rm -rf golang && mkdir -p golang
 
-$PROTOC_PATH -I . \
-  -I "$GOPATH"/src \
+$PROTOC_PATH \
+  -I "$GOPATH"/pkg/mod \
+  -I . \
   --gogo_out=plugins=grpc,paths=source_relative:golang \
   chargehive/**/*.proto
 
-# init
+# php - remove gogo registration from php
 mkdir -p genproto
 cp -R chargehive genproto
-
-# php
-rm -rf php
-mkdir -p php
-gsed -i'' -E 's#^import "github.com/gogo/protobuf/gogoproto/gogo.proto";$##g' genproto/chargehive/**/*.proto
+rm -rf php && mkdir -p php
+gsed -i'' -E 's#^import "github.com/gogo/protobuf([^/]+)?/gogoproto/gogo.proto";$##g' genproto/chargehive/**/*.proto
 gsed -i'' -E 's#^option \(gogoproto\..+##g' genproto/chargehive/**/*.proto
 
-$PROTOC_PATH -I genproto \
-  -I "$GOPATH"/src \
+$PROTOC_PATH \
+  -I "$GOPATH"/pkg/mod \
+  -I ./genproto \
   --php_out=php \
   genproto/chargehive/**/*.proto
 
--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/chargehive/proto
 go 1.12
 
 require (
-	github.com/gogo/protobuf v1.3.0 // indirect
-	github.com/golang/protobuf v1.3.2 // indirect
+	github.com/gogo/protobuf v1.3.0
+	github.com/golang/protobuf v1.5.0
+	google.golang.org/protobuf v1.27.1
 )
