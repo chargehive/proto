c2fa00d4c1d3fcb4406b1aec933248e98f6b962b
Tom Kay <oridan82@gmail.com>
2021-08-06 10:19:09 +0100

    use absolute paths

--- generate.sh
+++ generate.sh
@@ -24,22 +24,24 @@ fi
 
 go mod vendor
 
+# global - change absolute path to relative
+rm -rf tmp_php_proto && mkdir -p tmp_php_proto
+cp -R chargehive tmp_php_proto
+gsed -i'' -E 's#^import "github.com/chargehive/proto/chargehive/#import "chargehive/#g' tmp_php_proto/chargehive/**/*.proto
+
 # go
 rm -rf golang && mkdir -p golang
-
 $PROTOC_PATH \
   -I ./vendor \
-  -I . \
+  -I ./tmp_php_proto \
   --gogo_out=plugins=grpc,paths=source_relative:golang \
   chargehive/**/*.proto
 
 # php - remove gogo registration from php
-rm -rf tmp_php_proto && mkdir -p tmp_php_proto
-cp -R chargehive tmp_php_proto
-rm -rf php && mkdir -p php
 gsed -i'' -E 's#^import "github.com/gogo/protobuf/gogoproto/gogo.proto";$##g' tmp_php_proto/chargehive/**/*.proto
 gsed -i'' -E 's#^option \(gogoproto\..+##g' tmp_php_proto/chargehive/**/*.proto
 
+rm -rf php && mkdir -p php
 $PROTOC_PATH \
   -I ./vendor \
   -I ./tmp_php_proto \
