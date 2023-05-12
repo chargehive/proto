a1fa5d10aeea2d394a767fae212e1beebafa3809
Tom Kay <oridan82@gmail.com>
2023-05-12 19:00:27 +0100

    drop gogo, use protoc-gen-go-enums for backward compatible enum constants

--- generate.sh
+++ generate.sh
@@ -16,7 +16,7 @@ if [[ "$VER" != "$EXPECT" ]]; then
   #exit 1 # this is not a critical error
 fi
 
-GO_PLUGIN_PATH=$(command -v protoc-gen-gogo 2>&1)
+GO_PLUGIN_PATH=$(command -v protoc-gen-go-enums 2>&1)
 if [[ "$GO_PLUGIN_PATH" == "" ]]; then
   echo "'protoc-gen-go' tool is required but missing." >&2
   exit 1
@@ -24,30 +24,12 @@ fi
 
 protodep up
 
-# go
-(
-  rm -rf golang && mkdir -p golang
-  $PROTOC_PATH \
-    -I . \
-    -I ./protodep \
-    --gogo_out=plugins=grpc,paths=source_relative:golang \
-    ./chargehive/**/*.proto
-)
-
-# php - remove gogo registration from php
-(
-  rm -rf tmp_php_proto && mkdir -p tmp_php_proto
-  cp -R chargehive tmp_php_proto
-  gsed -i'' -E 's#^import "gogoproto/gogo.proto";$##g' tmp_php_proto/chargehive/**/*.proto
-  gsed -i'' -E 's#^option \(gogoproto\..+##g' tmp_php_proto/chargehive/**/*.proto
-
-  rm -rf php && mkdir -p php
-  $PROTOC_PATH \
-    -I ./tmp_php_proto \
-    -I ./protodep \
-    --php_out=php \
-    ./tmp_php_proto/chargehive/**/*.proto
-
-  # cleanup
-  rm -Rf tmp_php_proto
-)
+rm -rf golang && mkdir -p golang
+rm -rf php && mkdir -p php
+$PROTOC_PATH \
+  -I . \
+  -I ./protodep \
+  --php_out=php \
+  --go-enums_out=paths=source_relative:golang \
+  --go_out=paths=source_relative:golang \
+  ./chargehive/**/*.proto
